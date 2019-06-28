8b585192d6d89f7ab09aa68e82182afbfa00114b
Adrian Göransson <ag223ph@student.lnu.se>
2019-06-28 16:22:53 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float inAirSpeed;
+    public float jumpSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        bool inAir = transform.position.y > 0.51;
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        bool jumpPressed = Input.GetKeyDown(KeyCode.Space);
+
+        float factor = speed;
+
+        Rigidbody body = GetComponent<Rigidbody>();
+        Vector3 movement = new Vector3(moveH, 0, moveV);
+        Vector3 jump = new Vector3(0, 0, 0);
+
+        if (inAir) {
+            factor = inAirSpeed;
+        } else if (jumpPressed) {
+            jump.y = 10;
+        }
+
+        body.AddForce(movement * factor * Time.deltaTime);
+        body.AddForce(jump * jumpSpeed * Time.deltaTime);
+    }
+}
