6f4e1854ec4dfa0af1006012bd9e6221c8d59db6
Adrian Göransson <ag223ph@student.lnu.se>
2019-07-03 01:23:52 +0200

    Preliminary maze generation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject Wall;
+
     // Start is called before the first frame update
     void Start()
     {
+        MazeGenerator mg = new MazeGenerator();
 
+        int[,] maze = mg.GenerateMaze(30, 1230123);
+
+        for (int row = 0; row < maze.GetLength(0); row++) {
+            int x = row;
+
+            for (int column = 0; column < maze.GetLength(1); column++) {
+                int z = column;
+
+                if (maze[row, column] == 1) {
+                    Vector3 pos = new Vector3(x, Wall.transform.position.y, z);
+                    Instantiate(Wall, pos, Quaternion.identity);
+                }
+            }
+        }
+
+        Vector3 v = new Vector3(0, 0, 0);
+        Instantiate(Wall, v, Quaternion.identity);
     }
 
     // Update is called once per frame
