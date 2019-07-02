2cd17ce21158a899b07869f2bdae56239e628cd4
Adrian Göransson <ag223ph@student.lnu.se>
2019-07-02 22:41:17 +0200

    Maze generator

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class MazeGenerator
+{
+    // Slightly modified logic from https://www.raywenderlich.com/82-procedural-generation-of-mazes-with-unity
+    public int[,] GenerateMaze(int size, int seed)
+    {
+        Random rand = new Random(seed);
+
+        float placementThreshold = .1f;
+
+        int[,] maze = new int[size, size];
+
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        for (int i = 0; i <= rMax; i++) {
+            for (int j = 0; j <= cMax; j++) {
+                //1
+                if (i == 0 || j == 0 || i == rMax || j == cMax) {
+                    maze[i, j] = 1;
+                }
+
+                //2
+                else if (i % 2 == 0 && j % 2 == 0) {
+                    if (rand.NextDouble() > placementThreshold) {
+                        //3
+                        maze[i, j] = 1;
+
+                        int a = rand.NextDouble() < .5 ? 0 : (rand.NextDouble() < .5 ? -1 : 1);
+                        int b = a != 0 ? 0 : (rand.NextDouble() < .5 ? -1 : 1);
+                        maze[i + a, j + b] = 1;
+                    }
+                }
+            }
+        }
+
+        return maze;
+    }
+}
