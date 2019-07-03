0edf49f114527b11bbe8e218f42592638f2e6b31
Adrian Göransson <ag223ph@student.lnu.se>
2019-07-03 16:01:29 +0200

    maze gen: rename i,j to row,col

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -14,27 +14,27 @@ public class MazeGenerator
         int rowMax = maze.GetUpperBound(0);
         int columnMax = maze.GetUpperBound(1);
 
-        for (int i = 0; i <= rowMax; i++) {
-            for (int j = 0; j <= columnMax; j++) {
-                if (i < emptyPosition + emptySize && i > emptyPosition - emptySize &&
-                    j < emptyPosition + emptySize && j > emptyPosition - emptySize) {
+        for (int row = 0; row <= rowMax; row++) {
+            for (int col = 0; col <= columnMax; col++) {
+                if (row < emptyPosition + emptySize && row > emptyPosition - emptySize &&
+                    col < emptyPosition + emptySize && col > emptyPosition - emptySize) {
                     continue;
                 }
 
                 //1
-                if (i == 0 || j == 0 || i == rowMax || j == columnMax && rand.NextDouble() < .3) {
-                    maze[i, j] = 1;
+                if (row == 0 || col == 0 || row == rowMax || col == columnMax && rand.NextDouble() < .3) {
+                    maze[row, col] = 1;
                 }
 
                 //2
-                else if (i % 2 == 0 && j % 2 == 0) {
+                else if (row % 2 == 0 && col % 2 == 0) {
                     if (rand.NextDouble() > placementThreshold) {
                         //3
-                        maze[i, j] = 1;
+                        maze[row, col] = 1;
 
                         int a = rand.NextDouble() < .5 ? 0 : (rand.NextDouble() < .5 ? -1 : 1);
                         int b = a != 0 ? 0 : (rand.NextDouble() < .5 ? -1 : 1);
-                        maze[i + a, j + b] = 1;
+                        maze[row + a, col + b] = 1;
                     }
                 }
             }
@@ -43,7 +43,7 @@ public class MazeGenerator
         return maze;
     }
 
-    // Create a maze of `size` with `seed`. Create an empty room in the middle that is 5*5 blocks.
+    // Create a maze of `size` with `seed`. Create an empty room in the middle that is 3*3 blocks.
     public int[,] GenerateMaze(int size, int seed)
     {
         return GenerateMaze(size, seed, size / 2, 3);
