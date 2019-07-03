118a114d4ef4ec94baa7c76162d23dea31d962c6
Adrian Göransson <ag223ph@student.lnu.se>
2019-07-03 15:24:35 +0200

    Refactor mazegenerator to create an empty room in the middle

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -3,7 +3,7 @@
 public class MazeGenerator
 {
     // Slightly modified logic from https://www.raywenderlich.com/82-procedural-generation-of-mazes-with-unity
-    public int[,] GenerateMaze(int size, int seed)
+    public int[,] GenerateMaze(int size, int seed, int emptyPosition, int emptySize)
     {
         Random rand = new Random(seed);
 
@@ -11,13 +11,18 @@ public class MazeGenerator
 
         int[,] maze = new int[size, size];
 
-        int rMax = maze.GetUpperBound(0);
-        int cMax = maze.GetUpperBound(1);
+        int rowMax = maze.GetUpperBound(0);
+        int columnMax = maze.GetUpperBound(1);
 
-        for (int i = 0; i <= rMax; i++) {
-            for (int j = 0; j <= cMax; j++) {
+        for (int i = 0; i <= rowMax; i++) {
+            for (int j = 0; j <= columnMax; j++) {
+                if (i < emptyPosition + emptySize && i > emptyPosition - emptySize &&
+                    j < emptyPosition + emptySize && j > emptyPosition - emptySize) {
+                    continue;
+                }
+
                 //1
-                if (i == 0 || j == 0 || i == rMax || j == cMax && rand.NextDouble() < .3) {
+                if (i == 0 || j == 0 || i == rowMax || j == columnMax && rand.NextDouble() < .3) {
                     maze[i, j] = 1;
                 }
 
@@ -36,5 +41,11 @@ public class MazeGenerator
         }
 
         return maze;
+    }
+
+    // Create a maze of `size` with `seed`. Create an empty room in the middle that is 5*5 blocks.
+    public int[,] GenerateMaze(int size, int seed)
+    {
+        return GenerateMaze(size, seed, size / 2, 3);
     }
 }
