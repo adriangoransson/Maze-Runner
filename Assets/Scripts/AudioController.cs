404ca896f70b20fc17cf348b6bf90603dd65b058
Adrian Göransson <ag223ph@student.lnu.se>
2019-07-04 13:26:44 +0200

    Add Audio toggle prefab to ingame and main menu

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    private const string AUDIO_KEY = "AUDIO_KEY";
+
+    void Start()
+    {
+        Toggle t = GetComponent<Toggle>();
+        t.onValueChanged.AddListener(Toggled);
+
+        t.isOn = PlayerPrefs.GetInt(AUDIO_KEY, 1) > 0;
+    }
+
+    private void Toggled(bool soundOn)
+    {
+        int volume = soundOn ? 1 : 0;
+        AudioListener.volume = volume;
+        PlayerPrefs.SetInt(AUDIO_KEY, volume);
+        PlayerPrefs.Save();
+    }
+}
