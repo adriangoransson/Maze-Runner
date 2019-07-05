2c3c999a94f2fc089767a95c8ab48b7bd96f6db6
Adrian Göransson <ag223ph@student.lnu.se>
2019-07-05 19:11:46 +0200

    Clean up ingame menu controller and reference it from ui

--- Assets/Scripts/GameMenuController.cs
+++ Assets/Scripts/GameMenuController.cs
@@ -1,38 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameMenuController : MonoBehaviour
 {
-
-    private GameController gameController;
-
-    public Button resumeButton;
-    public Button restartButton;
-    public Button audioButton;
-    public Button mainMenuButton;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        GameObject go = GameObject.FindGameObjectWithTag("GameController");
-        gameController = go.GetComponent<GameController>();
-
-        resumeButton.onClick.AddListener(gameController.TogglePause);
-        restartButton.onClick.AddListener(RestartGame);
-
-        mainMenuButton.onClick.AddListener(GoToMainMenu);
-    }
-
-    void RestartGame()
+    public void RestartGame()
     {
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
     }
 
-    void GoToMainMenu()
+    public void GoToMainMenu()
     {
         SceneManager.LoadScene(0);
     }
