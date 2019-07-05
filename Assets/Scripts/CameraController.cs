ad9ef1abb0af95f4a6b52963393425454ee9cab9
Adrian Göransson <ag223ph@student.lnu.se>
2019-07-05 09:51:48 +0200

    Add camera rotation using the Q and E keys TODO:slerp?

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float lookAtOffset = 1.2f;
+    public float rotationSpeed = 3f;
     public GameObject player;
+
     private Vector3 offset;
+    private int rotation;
 
     // Start is called before the first frame update
     void Start()
@@ -13,8 +17,27 @@ public class CameraController : MonoBehaviour
         offset = transform.position - player.transform.position;
     }
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Q)) {
+            rotation = -1;
+        } else if (Input.GetKey(KeyCode.E)) {
+            rotation = 1;
+        } else {
+            rotation = 0;
+        }
+    }
+
     void LateUpdate()
     {
+        offset = Quaternion.AngleAxis(rotation * rotationSpeed, Vector3.up) * offset;
+
         transform.position = player.transform.position + offset;
+
+        Vector3 playerPos = player.transform.position;
+        // Look slightly above the ball
+        playerPos.y += lookAtOffset;
+
+        transform.LookAt(playerPos);
     }
 }
